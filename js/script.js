283385767da1041cbcaf227700217649da9d8bbb
bosciu <bosciu@icloud.com>
2021-05-18 14:29:08 +0200

    Creazione struttura base e collegamenti

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Gestire il cambio dell'immagine al click sui pallini in basso (nav);
+Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente. */
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        message: "Test"
+    }
+});
